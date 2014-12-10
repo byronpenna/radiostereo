8d8bbe037599f04da12681eb0f668237d77df3b5
kevin6911 <kevinj_1254@hotmail.com>
2014-12-10 15:54:30 -0600

    cotizacion
    Avance en diseño de la pantalla de cotizacion

--- application/views/login/login.php
+++ application/views/login/login.php
@@ -4,9 +4,8 @@
 	<?php 
 		$this->load->view("estructura/head.php");
 	?>
-<script  type="text/javascript" charset="utf-8">
-	
-</script>
+<script src="<?php echo base_url('resources/js/login/script.js')?>" type="text/javascript" charset="utf-8"></script>
+<script src="<?php echo base_url('resources/js/login/function.js')?>" type="text/javascript" charset="utf-8"></script>
 </head>
 <body>
             <div class="container">
@@ -15,17 +14,17 @@
                         <div class="panel panel-default">
                             <div class="panel-heading">                                
                                 <div class="row-fluid user-row">
-                                    <img src="<?php echo base_url('resources/imagenes/logo.png') ?>" class="img-responsive" alt="BISA"/>
+                                    <img src="<?php echo base_url('resources/imagenes/logo.png')?> " class="img-responsive" alt="BISA"/>
                                 </div>
                             </div>
                             <div class="panel-body">
-                                <form accept-charset="UTF-8" role="form" class="form-signin">
+                                <form accept-charset="UTF-8" role="form" class="form-signin" id="frmLogin">
                                     <fieldset>
                                         <label class="panel-login">
                                             <div class="login_result"></div>
                                         </label>
-                                        <input class="form-control" placeholder="Usuario" id="username" type="text">
-                                        <input class="form-control" placeholder="Contraseña" id="password" type="password">
+                                        <input class="form-control" placeholder="Usuario" id="username" name="txtUsuario" type="text">
+                                        <input class="form-control" placeholder="Contraseña" id="password" name="txtContra" type="password">
                                         <br></br>
                                         <input class="btn btn-lg btn-success btn-block" type="submit" id="login" value="Iniciar Sesion">
                                     </fieldset>
