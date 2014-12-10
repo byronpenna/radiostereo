7e8419b820b2d1e2d3812ef9c703e677a0bd8efa
kevin6911 <kevinj_1254@hotmail.com>
2014-12-10 14:55:56 -0600

    diseño de crear cotizacion
    diseño de creaar cotizacion

--- application/views/cotizacion/crearCotizacion.php
+++ application/views/cotizacion/crearCotizacion.php
@@ -10,7 +10,36 @@
 	<div class="container" id="contenedorClientes">
     <h3>Crear Cotizacion</h3>
     <hr id="hr">
-    
+    	<section id="mainCot">
+    		<article id="cotHeader">
+    			<article>
+    				<p>Id de Cliente <span>182913</span></p>
+    				<p>Nombre <span>Kevin Alberto Garcia Cortez</span></p>	
+    			</article>
+    			<article>
+    				<p>Tipo Cotizacion <span>
+    					<select name="" class="form-control input-sm">
+    						<option value="">Opcion 1</option>
+    						<option value="">Opcion 2</option>
+    					</select>
+    				</span></p>
+    				<p>Estado de Ctoizacion <span>
+    					<select name="" class="form-control input-sm">
+    						<option value="">Opcion 1</option>
+    						<option value="">Opcion 2</option>
+    					</select>
+    				</span></p>
+    			</article>
+    			<article>
+    				<p><br>Fecha de Creacion <span> <input type="date" name="" class="form-control input-sm"></span></p>
+    			</article>
+    		</article>
+    		<hr>
+			<article id="contCotSer">
+				asasasas
+			</article>
+    		
+    	</section>
     </div>
 </body>
 </html>
