f8a1305f4f23b135d404f935494d1209ea20b5eb
kevin6911 <kevinj_1254@hotmail.com>
2014-12-10 15:54:30 -0600

    cotizacion
    Avance en diseño de la pantalla de cotizacion

---
+++ resources/js/login/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+
+		$(document).on('submit','#frmLogin',function(e){
+			 e.preventDefault();
+			var frm= serializeToJson($(this).serializeArray());
+			console.log(frm);
+		});
+});
