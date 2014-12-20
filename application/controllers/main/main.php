e60ca3bddfdb73ead32a6e1fc7272369b75a13cf
kevin6911 <kevinj_1254@hotmail.com>
2014-12-19 20:08:51 -0600

    cambios kevin casa
    kevin cambios casa

--- application/controllers/main/main.php
+++ application/controllers/main/main.php
@@ -10,8 +10,10 @@
 				$this->load->model('mainm/mainm');
 				$mainm = new mainm();
 				$tabla = new stdClass();
-				$IdUser = $_SESSION['iduser'];
-				$tabla->clientes = $mainm->DatosCliente($IdUser);
+				if(isset($_SESSION['iduser'])){
+					$IdUser = $_SESSION['iduser'];	
+					$tabla->clientes = $mainm->DatosCliente($IdUser);
+				}
 				$data['tabla'] = $tabla;
 				$data["Titulo"] = "Principal";
 				$this->load->view("index/index.php",$data);
