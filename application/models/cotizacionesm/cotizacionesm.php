5c54f0a7b76b519531aa4ac02154b3a1ea19ed69
virgilio0512 <virgilio.antonioramos@gmail.com>
2015-01-09 11:33:33 -0600

    cambis sin querer

--- application/models/cotizacionesm/cotizacionesm.php
+++ application/models/cotizacionesm/cotizacionesm.php
@@ -361,20 +361,18 @@
                                         <input type="text" name="txtFechaInicio" value="'.$query[0]->enc_fecha_inicio.'" placeholder="aaaa-mm-dd" class="form-control input-sm medios  datepicker fi txtFechaInicio" required>
                                     </span>
                             </article> 
-                            <div id="contenedor1" class="conteCalendario">
-								<div class="calendar"></div><br>
-								<article id="BtnAddEvent">
-                					<input type="button" value="Guardar" class="btn btn-sm btn-success btnAddEvent">
-            					</article>
-							</div>
+                            
                             <article class="fechaFin" >
                                 <span >Fin de Pauta</span>    
                                 <span>
-                                    <input type="text" name="txtFechaFin" value="'.$query[0]->enc_fecha_fin.'" placeholder="aaaa-mm-dd" class="form-control input-sm medios fechaFin datepicker ffin" required>
+                                    <input type="text" name="txtFechaFin" value="'.$query[0]->enc_fecha_fin.'" placeholder="aaaa-mm-dd" class="form-control input-sm medios fechaFin datepicker ffin txtFechaFin" required>
                                 </span>
                             </article> 
                             <img src="'.base_url("resources/imagenes/calendario.png").'" class="imagen imagen1" />
-                            <input type=\'text\' value=\'\' class=\'txtEvents\'>   
+                            <input type=\'text\' value=\'\' class=\'txtEvents\'>  
+                            <div id="contenedor1" class="conteCalendario">
+								<div class="calendar"></div><br>
+							</div> 
                     </article>
                     </article>
                 </article>
--- resources/js/cotizacion/calendario/script.js
+++ resources/js/cotizacion/calendario/script.js
@@ -3,28 +3,22 @@ $(document).ready(function() {
 	
 	$(document).on("click",".imagen",function() {
 		fechaInicio = $(this).parents(".cuerpo").find(".txtFechaInicio").val();
+		fechaFin = $(this).parents(".cuerpo").find(".txtFechaFin").val();
+		//fi = $.datepicker.parseDate('YYYY-MM-DD', fechaInicio );
+		ff = Date.parse(fechaFin);
+		eventos = $(this).parents(".cuerpo").find(".txtEvents").val()
+		var meses = new Array ("Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre");
+		var diasSemana = new Array("D","L","M","M","J","V","S");
+		for (var i = 0; i <= diasSemana.length-1; i++) {
+			txtEvento.val(txtEvento.val().replace(diasSemana[i]));
+		};
 		eventos = $(this).parents(".cuerpo").find(".txtEvents").val();//localizo los eventos guardados en el textbox
-		eventos = eventos.split(",");//separar los en forma de arreglo x comas
-		var eventosCalendar;
-		if($(this).parents(".cuerpo").find(".txtEvents").val() != ""){
-			eventosCalendar = new Array();
-			$.each(eventos,function(i,val){//construyo el arreglo de objetos
-				eventosCalendar[i] = new Object();
-				//eventosCalendar[i].title = null;
-				eventosCalendar[i].start = val;
-				eventosCalendar[i].overlap= false;
-				eventosCalendar[i].color = "red";
-			});
-		}else{
-			eventosCalendar = null;
-		}
-		console.log("el arreglo de eventos es: ",eventosCalendar);
+		//fechaInicio = fechaInicio.split(",");//separar los en forma de arreglo x comas
 		txtEvento= $(this).parents(".cuerpo").find(".txtEvents");
 		console.log(eventos);
-		//console.log("la fecha de inicio es: ",fechaInicio);
+		//console.log("la fecha de inicio es: ",fi, " y la fecha fin es: ",ff);
 		if (fechaInicio != "") {
-			$("#contenedor1").show();
-			getCalendar($(".calendar"),fechaInicio,eventosCalendar,txtEvento);//mando a llamar a la funcion q me muestra el calendario que esta en funchtion.js
+			
 			$("#contenedor1").bPopup({//codigo para el popup
 		            easing: 'easeOutBack', //uses jQuery easing plugin
 		            opacity: 0.6,
